f2d5ff523cd0cf3a499943422de3b9c348c40202
mkl_dnn <mkl_dnn@intel.com>
2017-10-30 18:32:17 +0300

    gh-pages: update documentation to 682b049

--- search/all_61.js
+++ search/all_61.js
@@ -7,6 +7,5 @@ var searchData=
   ['any',['any',['../group__cpp__api__memory.html#gga81bcf1ea92d7f98852a2c3e187825de6af65460ef562d540de287f58445d54748',1,'mkldnn::engine::any()'],['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea986f069b593c7c6610bc15d678e653d9',1,'mkldnn::memory::any()'],['../group__cpp__api__memory.html#gga7b46a4cd837ea0ccf835d7fa4fc37cffa488e6cebf53697050c0b2cc3829aeb94',1,'mkldnn::stream::any()']]],
   ['at',['at',['../structmkldnn_1_1primitive_1_1at.html',1,'mkldnn::primitive']]],
   ['at',['at',['../group__cpp__api__memory.html#ga1cd6d53f6696650f834559d2b28d1d5d',1,'mkldnn::primitive::at']]],
-  ['auxiliary_20types_20for_20memory_20description',['Auxiliary types for memory description',['../group__c__api__types__memory.html',1,'']]],
-  ['a_20performance_20library_20for_20deep_20learning',['A Performance Library for Deep Learning',['../index.html',1,'']]]
+  ['auxiliary_20types_20for_20memory_20description',['Auxiliary types for memory description',['../group__c__api__types__memory.html',1,'']]]
 ];
--- search/all_6c.js
+++ search/all_6c.js
@@ -3,7 +3,6 @@ var searchData=
   ['lrn',['LRN',['../group__c__api__lrn.html',1,'']]],
   ['layout_5fdesc',['layout_desc',['../structmkldnn__memory__desc__t.html#ae49f98c84c36039ec2984c3ecf981c8e',1,'mkldnn_memory_desc_t']]],
   ['lazy',['lazy',['../group__cpp__api__memory.html#gga7b46a4cd837ea0ccf835d7fa4fc37cffa02b04eb4c732cc0525e409b7e7e60498',1,'mkldnn::stream']]],
-  ['legal_20information',['Legal Information',['../legal_information.html',1,'index']]],
   ['legal_5finformation_2emd',['legal_information.md',['../legal__information_8md.html',1,'']]],
   ['local_5fsize',['local_size',['../structmkldnn__lrn__desc__t.html#a6e4711d09762d5c3a38dac99ab4397df',1,'mkldnn_lrn_desc_t']]],
   ['lrn_5facross_5fchannels',['lrn_across_channels',['../group__cpp__api__memory.html#ggae45a07d6121fdd33e310782753178076a5ba3a03af8dc6c1b9066e48508ae9b5c',1,'mkldnn']]],
@@ -12,8 +11,9 @@ var searchData=
   ['lrn_5fbackward',['lrn_backward',['../structmkldnn_1_1lrn__backward.html',1,'mkldnn']]],
   ['lrn_5fbeta',['lrn_beta',['../structmkldnn__lrn__desc__t.html#ad29c3af8cae0dc599c62d3e04ebb94df',1,'mkldnn_lrn_desc_t']]],
   ['lrn_5fd',['lrn_d',['../group__cpp__api__engine.html#gga6d88ff11a07bae09a5c348d314c5d1d9a76add43bb6bee4f897e668e2d54476a2',1,'mkldnn']]],
-  ['lrn_5fforward',['lrn_forward',['../group__cpp__api__memory.html#ga452b11f3734755695cdf6eb4d4556a84',1,'mkldnn::lrn_forward::lrn_forward(const primitive_desc &amp;aprimitive_desc, const primitive::at &amp;src, const memory &amp;workspace, const memory &amp;dst)'],['../group__cpp__api__memory.html#gacbd49285d69e89a49bab508d096b27c2',1,'mkldnn::lrn_forward::lrn_forward(const primitive_desc &amp;aprimitive_desc, const primitive::at &amp;src, const memory &amp;dst)']]],
   ['lrn_5fforward',['lrn_forward',['../structmkldnn_1_1lrn__forward.html',1,'mkldnn']]],
+  ['lrn_5fforward',['lrn_forward',['../group__cpp__api__memory.html#ga452b11f3734755695cdf6eb4d4556a84',1,'mkldnn::lrn_forward::lrn_forward(const primitive_desc &amp;aprimitive_desc, const primitive::at &amp;src, const memory &amp;workspace, const memory &amp;dst)'],['../group__cpp__api__memory.html#gacbd49285d69e89a49bab508d096b27c2',1,'mkldnn::lrn_forward::lrn_forward(const primitive_desc &amp;aprimitive_desc, const primitive::at &amp;src, const memory &amp;dst)']]],
   ['lrn_5fk',['lrn_k',['../structmkldnn__lrn__desc__t.html#a87736331128ad8a787012b6f3eced386',1,'mkldnn_lrn_desc_t']]],
-  ['lrn_5fwithin_5fchannel',['lrn_within_channel',['../group__cpp__api__memory.html#ggae45a07d6121fdd33e310782753178076a1bec35024f741400d95f4c15897b50a2',1,'mkldnn']]]
+  ['lrn_5fwithin_5fchannel',['lrn_within_channel',['../group__cpp__api__memory.html#ggae45a07d6121fdd33e310782753178076a1bec35024f741400d95f4c15897b50a2',1,'mkldnn']]],
+  ['legal_20information',['Legal Information',['../md_doc_legal_information.html',1,'']]]
 ];
