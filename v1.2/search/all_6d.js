d0ce0b9f0175a3e23caed531ba80d71dc6cbab4c
mkl_dnn <mkl_dnn@intel.com>
2020-01-14 00:26:03 +0300

    gh-pages: update documentation

---
+++ v1.2/search/all_6d.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['matmul_20tutorial_3a_20quantization',['MatMul Tutorial: Quantization',['../cpu_matmul_quantization_cpp.html',1,'']]],
+  ['matmul_20tutorial_3a_20comparison_20with_20sgemm',['MatMul Tutorial: Comparison with SGEMM',['../cpu_sgemm_and_matmul_cpp.html',1,'']]],
+  ['matrix_20multiplication',['Matrix Multiplication',['../dev_guide_matmul.html',1,'']]],
+  ['matrix_20multiplication',['Matrix Multiplication',['../group__dnnl__api__matmul.html',1,'']]],
+  ['memory',['Memory',['../group__dnnl__api__memory.html',1,'']]],
+  ['matmul_20tutorial_3a_20int8_20inference',['MatMul Tutorial: INT8 Inference',['../inference_int8_matmul_cpp.html',1,'']]],
+  ['major',['major',['../structdnnl__version__t.html#ab0051e104a0b84b25c22f2ed98a9a16c',1,'dnnl_version_t']]],
+  ['map_5fdata',['map_data',['../structdnnl_1_1memory.html#ad11ae4ffa08379b58eb096d15304739a',1,'dnnl::memory']]],
+  ['matmul',['matmul',['../structdnnl_1_1matmul.html#af1466fb3b36d0f10e59b41e47e940b02',1,'dnnl::matmul::matmul()=default'],['../structdnnl_1_1matmul.html#aa1052f851d887db245f58f014cd6b0f5',1,'dnnl::matmul::matmul(const primitive_desc &amp;pd)'],['../structdnnl_1_1primitive.html#ad1ec93215a0cf3aa0a32bae0c2cd9169a0f2e1c85f0064ca22d3ac468c44b0327',1,'dnnl::primitive::matmul()']]],
+  ['matmul',['matmul',['../structdnnl_1_1matmul.html',1,'dnnl']]],
+  ['matmul_5fd',['matmul_d',['../group__dnnl__api__primitives__common.html#gga94efdd650364f4d9776cfb9b711cbdc1a4a7fa4b07a2f5ef2534d7159225f480d',1,'dnnl']]],
+  ['mean_5fdesc',['mean_desc',['../structdnnl_1_1batch__normalization__forward_1_1primitive__desc.html#a55fa98fcb8e8a16c737542fed247f2f5',1,'dnnl::batch_normalization_forward::primitive_desc::mean_desc()'],['../structdnnl_1_1batch__normalization__backward_1_1primitive__desc.html#a3e723e73fae6bfe7344d0eaa7eee5638',1,'dnnl::batch_normalization_backward::primitive_desc::mean_desc()'],['../structdnnl_1_1layer__normalization__forward_1_1primitive__desc.html#a60b7145665746a639881e969c2e92e8c',1,'dnnl::layer_normalization_forward::primitive_desc::mean_desc()'],['../structdnnl_1_1layer__normalization__backward_1_1primitive__desc.html#a93af1823ce103760a0ee90b7ce93d83f',1,'dnnl::layer_normalization_backward::primitive_desc::mean_desc()']]],
+  ['memory',['memory',['../structdnnl_1_1memory.html',1,'dnnl']]],
+  ['memory',['memory',['../structdnnl__exec__arg__t.html#a048f23e80b923636267c4dece912cd0d',1,'dnnl_exec_arg_t::memory()'],['../structdnnl_1_1memory.html#a7349cd8dd17e53fbde7648fd7dc108a9',1,'dnnl::memory::memory(const desc &amp;md, const engine &amp;engine, void *handle)'],['../structdnnl_1_1memory.html#aa28e9a81e89a97b90b49535dc3a5c96e',1,'dnnl::memory::memory(const desc &amp;md, const engine &amp;engine)']]],
+  ['memory_5fconsumption_5fs64',['memory_consumption_s64',['../group__dnnl__api__primitives__common.html#gga94efdd650364f4d9776cfb9b711cbdc1a0ed44d67e94c1c7ac5f219491e422506',1,'dnnl']]],
+  ['memory_20format_20propagation',['Memory format propagation',['../memory_format_propagation_cpp.html',1,'']]],
+  ['minor',['minor',['../structdnnl__version__t.html#a30f17c81ffa45f1929bbe4f2c14fe42f',1,'dnnl_version_t']]]
+];
