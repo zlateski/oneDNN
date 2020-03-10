4a52e1b04a0b9c3689577dea9976887f4e1704c2
mkl_dnn <mkl_dnn@intel.com>
2020-03-11 01:10:02 +0300

    gh-pages: update documentation

---
+++ v1.3/search/functions_71.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['query',['query',['../structdnnl_1_1engine.html#a0979d6e10ff58fb9fe57302d8b9cdd00',1,'dnnl::engine']]],
+  ['query_5fmd',['query_md',['../structdnnl_1_1primitive__desc__base.html#ac2ec36092718ed89bece0fec7f5f3171',1,'dnnl::primitive_desc_base']]],
+  ['query_5fs64',['query_s64',['../structdnnl_1_1primitive__desc__base.html#aa3936cde6aa188f79cc379200de718b3',1,'dnnl::primitive_desc_base']]]
+];
